78e1708a53b9b43877fc485847719d03e6bab924
Dane4kaSH <shevchenko190605@mail.ru>
2024-06-11 11:00:35 +0300

    Complete

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]); // Вычисляем количество элементов в массиве
+
+    printf("Количество элементов в массиве: %d\n", size);
+
     return 0;
 }
